5f77270830f4ebc8842c1a843dc636bb15af42a8
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-01-19 07:21:05 +0200

    Create Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-item">
+                <img className="footer-icon" alt="footer-img" src="geolocation-favicon.png"></img>
+                <p className="copyright"> Апликация за търсене на адреси &copy; 2023 </p>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
